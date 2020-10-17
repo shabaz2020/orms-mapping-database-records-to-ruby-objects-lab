fa46c55a6e3f2e558450a46f83609094af935b67
shabaz2020 <shabaz8010@gmail.com>
2020-10-16 22:21:22 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,40 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    s = DB[:conn].execute(sql)
+    s.map do |i|
+      self.new_from_db(i)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students WHERE name = ?
+      SQL
+
+      DB[:conn].execute(sql, name).collect do |row|
+        self.new_from_db(row)
+      end.first
   end
-  
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
@@ -41,3 +63,37 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
+def self.all_students_in_grade_9
+  sql = <<-SQL
+    SELECT COUNT(grade = 9) FROM students
+  SQL
+
+  DB[:conn].execute(sql)
+end
+
+def self.students_below_12th_grade
+  sql = <<-SQL
+    SELECT COUNT(grade > 10) FROM students
+  SQL
+
+  s = DB[:conn].execute(sql, x)
+end
+
+def self.first_student_in_grade_10(x)
+  sql = <<-SQL
+    SELECT COUNT(grade > 10) FROM students
+  SQL
+  DB[:conn].execute(sql)
+end
+
+def self.all_students_in_grade_X(grade)
+  sql = <<-SQL
+    SELECT * FROM studnets WHERE grade = ? 
+  SQL
+
+  DB[:conn].execute(sql, grade)
+end
+end
+
+
